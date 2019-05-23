ea8f481041d084c19f37955d0493b413a689755f
MacMacky <mac21macky@gmail.com>
2019-05-23 14:11:08 +0800

    Change func naming convention 'camelCase' to 'snake_case'

--- algos/challenges_100.py
+++ algos/challenges_100.py
@@ -1,5 +1,5 @@
 
-def allLongestStrings(arr=[]):
+def all_longest_strings(arr=[]):
     lenArr = len(arr)
     if lenArr == 0:
         return arr
@@ -8,7 +8,7 @@ def allLongestStrings(arr=[]):
     return [item for item in arr if len(item) == max(lenStrs)]
 
 
-def evenDigitsOnly(n=1):
+def even_digits_only(n=1):
     digits = list(str(n))
     for digit in digits:
         if int(digit) % 2 == 1:
@@ -16,14 +16,14 @@ def evenDigitsOnly(n=1):
     return True
 
 
-def factorializeANumber(num=1):
+def factorialize_a_number(num=1):
     if num == 1:
         return 1
     else:
-        return num * factorializeANumber(num-1)
+        return num * factorialize_a_number(num-1)
 
 
-def reverseAString(string=""):
+def reverse_a_string(string=""):
     listo = list(string)
     listo.reverse()
     reversedStr = ""
@@ -32,15 +32,15 @@ def reverseAString(string=""):
     return reversedStr
 
 
-def reverseAString2(string=""):
+def reverse_a_string2(string=""):
     return string[::-1]
 
 
-def reverseAString3(string=""):
+def reverse_a_string3(string=""):
     return "".join(reversed(string))
 
 
-def isTandemRepeat(string=""):
+def is_tandem_repeat(string=""):
     half = len(string)//2
     return string[0:half] == string[half:len(string)]
 
@@ -50,29 +50,29 @@ def allLongestStrings(arr=[]):
     return [string for string in arr if len(string) == maxLen]
 
 
-def arrayReplace(arr=[], elementToReplace=0, substitutionElement=0):
+def array_replace(arr=[], elementToReplace=0, substitutionElement=0):
     if len(arr) == 0 or arr == None:
         return []
     return [(substitutionElement if el == elementToReplace else el) for el in arr]
 
 
-def largestOfFour(arrays=[[]]):
+def largest_of_four(arrays=[[]]):
     return [max(arr) for arr in arrays]
 
 
-def confirmEnding(string="", target=""):
+def confirm_ending(string="", target=""):
     return True if string.find(target) >= 0 else False
 
 
-def largestNumber(n=1):
+def largest_number(n=1):
     return 9 if n == 1 else int("9" * n)
 
 
-def properNounCorrection(noun=""):
+def proper_noun_correction(noun=""):
     return noun if noun.istitle() else (noun[0].upper() + noun[1:].lower())
 
 
-def domainTypes(domains=[]):
+def domain_types(domains=[]):
     length = len(domains)
     if length == 0 or domains is None:
         return []
@@ -94,20 +94,20 @@ def domainTypes(domains=[]):
         return result
 
 
-def addTwoDigits(num=0):
+def add_two_digits(num=0):
     if type(num) is not int:
         return 0
     else:
         return sum([int(char) for char in list(str(num))])
 
 
-def addNumbers(*args):
+def add_numbers(*args):
     return sum(args)
 
 
-def checkPalindrome(string=""):
-    return reverseAString2(string) == string
+def check_palindrome(string=""):
+    return reverse_a_string2(string) == string
 
 
-def findMax(arr=[]):
+def find_max(arr=[]):
     return max(arr)

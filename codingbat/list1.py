86a21136ad5361c4a23bd2c8a2fc87ba95fe23f6
MacMacky <mac21macky@gmail.com>
2019-05-25 12:51:40 +0800

    Add new func 'same_first_last' on 'list1' codingbat

--- codingbat/list1.py
+++ codingbat/list1.py
@@ -3,3 +3,14 @@ def first_last6(nums=[]):
         return False
     else:
         return True if nums[0:1][0] == 6 or nums[len(nums)-1:len(nums)][0] == 6 else False
+
+
+def same_first_last(nums=[]):
+    lenN = len(nums)
+    if lenN == 0:
+        return False
+    else:
+        return True if nums[0:1] == nums[lenN-1:lenN] else False
+
+
+print(same_first_last([1, 2, 4]))

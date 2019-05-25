f324f950c7633669c47f0720743c6c6b2841a24d
MacMacky <mac21macky@gmail.com>
2019-05-25 09:30:00 +0800

    Add new file 'string1.py' for codingbat

---
+++ codingbat/string1.py
@@ -0,0 +1,12 @@
+
+def hello_name(name=""):
+    return "Hello " + name + "1"
+
+
+def make_abba(a="", b=""):
+    return a + b + b + a
+
+
+def make_tags(tag="", word=""):
+    return '<' + tag + '>' + word + '</' + tag + '>'
+

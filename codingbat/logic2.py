f55be0b731981f03eea4ca91446a813dc5db475c
MacMacky <mac21macky@gmail.com>
2019-05-27 16:35:35 +0800

    Add new func 'no_teen_sum' on 'logic2' codingbat

--- codingbat/logic2.py
+++ codingbat/logic2.py
@@ -9,6 +9,16 @@ def lucky_sum(a=0, b=0, c=0):
         return a+b+c
 
 
-print(lucky_sum(1, 2, 3))
+def fix_teen(n=0):
+    return 0 if ((n >= 13 and n <= 19) and (n != 16 and n != 15)) else n
+
+
+def no_teen_sum(a=0, b=0, c=0):
+    return fix_teen(a) + fix_teen(b) + fix_teen(c)
+
+
+# print(no_teen_sum(1, 2, 3))
+# print(no_teen_sum(2, 13, 1))
+# print(no_teen_sum(2, 1, 14))
 # print(lucky_sum(1, 2, 13))
 # print(lucky_sum(1, 13, 3))

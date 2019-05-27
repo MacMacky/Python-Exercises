c9d41223a2df2671eb7692a628e8ea9bfb9a6ad6
MacMacky <mac21macky@gmail.com>
2019-05-27 16:28:34 +0800

    Add new func 'lucky_sum' on 'logic2' codingbat

---
+++ codingbat/logic2.py
@@ -0,0 +1,14 @@
+def lucky_sum(a=0, b=0, c=0):
+    if a == 13:
+        return 0
+    elif b == 13:
+        return a
+    elif c == 13:
+        return a + b
+    else:
+        return a+b+c
+
+
+print(lucky_sum(1, 2, 3))
+# print(lucky_sum(1, 2, 13))
+# print(lucky_sum(1, 13, 3))

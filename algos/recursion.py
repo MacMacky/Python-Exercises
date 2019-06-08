3350e291c601dedc052ead176908011e6c5d148e
Mark Abeto <mac21macky@gmailcom>
2019-06-08 12:31:55 +0800

    Add new func 'is_palindrome' on recursion and add corresponding tests

--- algos/recursion.py
+++ algos/recursion.py
@@ -1,2 +1,13 @@
 def factorial(n=1):
     return 1 if ((n == 0) or (n == 1)) else n * factorial(n-1)
+
+
+def is_palindrome(string=""):
+    length = len(string)
+    if length == 0:
+        return True
+    else:
+        if string[0] == string[length-1]:
+            return is_palindrome(string[1:length-1])
+        else:
+            return False
--- index.py
+++ index.py
@@ -1,10 +1,10 @@
 from algos.challenges_100 import domain_types, first_duplicate, sum_all_primes, is_prime, chunk_monkey, largest_number, proper_noun_correction, confirm_ending, all_longest_strings, largest_of_four, array_replace, even_digits_only, factorialize_a_number, reverse_a_string, is_tandem_repeat, reverse_a_string2, reverse_a_string3, add_two_digits, add_numbers, check_palindrome, find_max
 from algos.others import splitString
 from helpers.helpers import test, describe
-from algos.recursion import factorial
+from algos.recursion import factorial, is_palindrome
 
 test('factorial_recursion', factorial(5), 120)
-
+test('is_palindrome_recursion', is_palindrome('aba'), True)
 
 # test("first_duplicate", first_duplicate([2, 3, 4, 5, 1]), -1)
 # test("first_duplicate", first_duplicate([2, 1, 3, 5, 3, 2]), 3)
